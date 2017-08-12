f96d564ce7637749eb5855dd68674ffaf2d8b8d3
Cody A. Coleman <codyaustun@gmail.com>
2017-08-11 20:28:33 -0700

    Added real data and extra columns

--- render.py
+++ render.py
@@ -1,9 +1,30 @@
 import os
 import json
 from glob import glob
+from collections import namedtuple
 
 import click
 from jinja2 import Template
+
+
+KEY_MAP = {
+    'time': 'trainingTime',
+    'cost': 'cost',
+    'latency': 'latency',
+    'throughput': 'batchThroughput'
+}
+
+
+Threshold = namedtuple('Threshold', ['field', 'cutoff'])
+
+
+TASK_THRESHOLDS = {
+    'CIFAR10': Threshold('accuracy', 93),
+    'SQuAD': Threshold('f1Score', 70)
+}
+
+
+NULLS = ['None', 'N/A', 'null']
 
 
 @click.command()
@@ -13,12 +34,19 @@ from jinja2 import Template
 @click.option('--html-dir', '-h', default='leaderboard')
 @click.option('--results-dir', '-r', default='results')
 @click.option('--templates-dir', '-t', default='templates')
-@click.option('--key', '-k', default='time')
 @click.option('--limit', '-l', default=10)
-def render(task, metric, dest, html_dir, results_dir, templates_dir, key,
-           limit):
+@click.option('--reverse', is_flag=True)
+def render(task, metric, dest, html_dir, results_dir, templates_dir,
+           limit, reverse):
 
-    source = os.path.join(results_dir, task, metric)
+    if metric in ['time', 'cost']:
+        mode = 'train'
+    elif metric in ['throughput', 'latency']:
+        mode = 'inference'
+    else:
+        raise NotImplementedError(f'Unknown metric: {metric}')
+
+    source = os.path.join(results_dir, task, mode)
     template = os.path.join(templates_dir, task, f"{metric}.html")
     dest = dest or os.path.join(html_dir, task, f"{metric}.html")
 
@@ -26,9 +54,10 @@ def render(task, metric, dest, html_dir, results_dir, templates_dir, key,
     print(f"Template: {template}")
     print(f"Output: {dest}")
 
-    if not os.path.exists(dest):
+    if not os.path.exists(os.path.dirname(dest)):
         os.makedirs(os.path.dirname(dest))
 
+    # Load
     paths = glob(os.path.join(source, "*.json"))
     print("Found {} submissions".format(len(paths)))
     submissions = []
@@ -37,7 +66,21 @@ def render(task, metric, dest, html_dir, results_dir, templates_dir, key,
             record = json.load(file)
             submissions.append(record)
 
-    submissions = sorted(submissions, key=lambda sub: sub[key])
+    key = KEY_MAP[metric]
+    threshold = TASK_THRESHOLDS[task]
+
+    # Filter
+    def filter_submission(submission):
+        return ((submission[key] is not None) and
+                (submission[key] not in NULLS) and
+                (submission[threshold.field] > threshold.cutoff))
+
+    threshold = TASK_THRESHOLDS[task]
+    submissions = list(filter(filter_submission, submissions))
+
+    # Rank
+    submissions = sorted(submissions, key=lambda sub: sub[key],
+                         reverse=reverse)
     submissions = submissions[:limit]
     for index, submission in enumerate(submissions):
         submission['rank'] = index + 1
---
+++ update.sh
@@ -0,0 +1,9 @@
+python render.py CIFAR10 time --limit 20
+python render.py CIFAR10 cost --limit 20
+python render.py CIFAR10 throughput --limit 20 --reverse
+python render.py CIFAR10 latency --limit 20
+
+python render.py SQuAD time --limit 20
+python render.py SQuAD cost --limit 20
+python render.py SQuAD throughput --limit 20 --reverse
+python render.py SQuAD latency --limit 20
